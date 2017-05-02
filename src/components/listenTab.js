f52e33a1361a066b578e82ced0f4e3a3c5973bdc
Gilan Salehi <Gilan Salehi>
2017-05-02 13:55:51 -0400

    Add Dance on Fire to Listen Tab

--- src/components/listenTab.js
+++ src/components/listenTab.js
@@ -7,7 +7,11 @@ export default class Listen extends Component {
         <h2 style={{fontFamily:'"Cinzel Decorative", cursive', textDecoration: 'underline'}}>
           Music
         </h2>
+        // Dance on Fire
+        <iframe width="100%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/304139776&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+        // Come Down
         <iframe width="100%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/314633983&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+        // We Built This City
         <iframe width="100%" height="200" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/102996468&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
       </div>
     );
